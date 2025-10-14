086d3f3b8c8a700e345220a043d94c71223010df
Anurag Rajput <code.annu@gmail.com>
2025-10-14 14:01:17 +0530

    implement seller profile feature

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import { authRouter } from "./api/router/auth-router";
 import { validateAuthorization } from "./api/middleware/auth-middleware";
+import { sellerRouter } from "./api/router/seller-router";
+import { validateRequestBody } from "./api/middleware/validate-request-body";
 
 const app = express();
 const BASE_URL = "/api/v1";
@@ -9,6 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(`${BASE_URL}/auth`, authRouter);
+app.use(`${BASE_URL}/seller`, validateAuthorization, sellerRouter);
 
 
 const PORT = 3000;
