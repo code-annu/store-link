0e49e5323a5bf0b24fa1e0d768ca477addbc3750
Anurag Rajput <code.annu@gmail.com>
2025-10-15 21:43:02 +0530

    implement store feature

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,7 +2,7 @@ import express from "express";
 import { authRouter } from "./api/router/auth-router";
 import { validateAuthorization } from "./api/middleware/auth-middleware";
 import { sellerRouter } from "./api/router/seller-router";
-import { validateRequestBody } from "./api/middleware/validate-request-body";
+import { storeRouter } from "./api/router/store-router";
 
 const app = express();
 const BASE_URL = "/api/v1";
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(`${BASE_URL}/auth`, authRouter);
 app.use(`${BASE_URL}/seller`, validateAuthorization, sellerRouter);
-
+app.use(`${BASE_URL}/stores`, storeRouter);
 
 const PORT = 3000;
 app.listen(PORT, () => {
