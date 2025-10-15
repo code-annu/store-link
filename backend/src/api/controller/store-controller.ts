9b41aa6f1565c1cc34e7b5976b8ddac0e26c1c4b
Anurag Rajput <code.annu@gmail.com>
2025-10-15 21:42:38 +0530

    implement api layer for store feature

---
+++ backend/src/api/controller/store-controller.ts
@@ -0,0 +1,96 @@
+import { Request, Response } from "express";
+import { CreateNewStore } from "../../domain/usecase/store/create-new-store-usecase";
+import { GetStoresOfOwner } from "../../domain/usecase/store/get-stores-of-owner-usecase";
+import { UpdateStore } from "../../domain/usecase/store/update-store-usecase";
+import { GetStore } from "../../domain/usecase/store/get-store-usecase";
+import { DeleteStore } from "../../domain/usecase/store/delete-store-usecase";
+import { IUserRepository } from "../../domain/repository/iuser-repository";
+import { IStoreRepository } from "../../domain/repository/istore-repository";
+import { AuthRequest } from "../middleware/auth-middleware";
+import { CustomError } from "../../domain/error/custom-error";
+import { ErrorType } from "../../domain/error/error-type";
+
+export class StoreController {
+  private createStore: CreateNewStore;
+  private getOwnerStores: GetStoresOfOwner;
+  private updateStore: UpdateStore;
+  private getStoreDetails: GetStore;
+  private deleteStoreDetails: DeleteStore;
+
+  constructor(storeRepo: IStoreRepository, userRepo: IUserRepository) {
+    this.createStore = new CreateNewStore(storeRepo, userRepo);
+    this.getOwnerStores = new GetStoresOfOwner(storeRepo, userRepo);
+    this.updateStore = new UpdateStore(storeRepo);
+    this.getStoreDetails = new GetStore(storeRepo);
+    this.deleteStoreDetails = new DeleteStore(storeRepo);
+  }
+
+  async postStore(req: AuthRequest, res: Response) {
+    try {
+      const data = req.body;
+      const uid = req.auth?.userId;
+      const store = await this.createStore.execute(uid!, data);
+      res.status(201).json(store);
+    } catch (e) {
+      if (e instanceof CustomError) {
+        res.status(e.errorType).json({ message: e.message });
+      } else {
+        res.status(500).json({ message: (e as Error).message });
+      }
+    }
+  }
+
+  async getStore(req: Request, res: Response) {
+    try {
+      const { storeUid } = req.params;
+      if (storeUid == null) {
+        throw new CustomError("Store path is required", ErrorType.BAD_REQUEST);
+      }
+      const store = await this.getStoreDetails.execute(storeUid);
+      res.status(200).json(store);
+    } catch (e) {
+      if (e instanceof CustomError) {
+        res.status(e.errorType).json({ message: e.message });
+      } else {
+        res.status(500).json({ message: (e as Error).message });
+      }
+    }
+  }
+
+  async patchStore(req: AuthRequest, res: Response) {
+    try {
+      const updates = req.body;
+      const uid = req.auth?.userId;
+      const { storeUid } = req.params;
+      if (storeUid == null) {
+        throw new CustomError("Store path is required", ErrorType.BAD_REQUEST);
+      }
+      const store = await this.updateStore.execute(uid!, storeUid, updates);
+      res.status(200).json(store);
+    } catch (e) {
+      if (e instanceof CustomError) {
+        res.status(e.errorType).json({ message: e.message });
+      } else {
+        res.status(500).json({ message: (e as Error).message });
+      }
+    }
+  }
+
+  async deleteStore(req: AuthRequest, res: Response) {
+    try {
+      const uid = req.auth?.userId;
+      const { storeUid } = req.params;
+      if (storeUid == null) {
+        throw new CustomError("Store path is required", ErrorType.BAD_REQUEST);
+      }
+      const store = await this.deleteStoreDetails.execute(uid!, storeUid);
+      res.status(200).json(store);
+    } catch (e) {
+      if (e instanceof CustomError) {
+        res.status(e.errorType).json({ message: e.message });
+      } else {
+        res.status(500).json({ message: (e as Error).message });
+      }
+    }
+  }
+}
---
+++ backend/src/api/router/store-router.ts
@@ -0,0 +1,40 @@
+import { Router } from "express";
+import { StoreController } from "../controller/store-controller";
+import { StoreRepository } from "../../infrastructure/repository/store-repository";
+import { SellerRepository } from "../../infrastructure/repository/seller-repository";
+import { UserRepository } from "../../infrastructure/repository/user-repository";
+import { validateAuthorization } from "../middleware/auth-middleware";
+import { validateRequestBody } from "../middleware/validate-request-body";
+import { StoreCreateSchema, StoreUpdateSchema } from "../schema/store-schema";
+
+export const storeRouter = Router({ mergeParams: true });
+
+const storeController = new StoreController(
+  new StoreRepository(),
+  new UserRepository()
+);
+
+storeRouter.post(
+  "/",
+  validateAuthorization,
+  validateRequestBody(StoreCreateSchema),
+  storeController.postStore.bind(storeController)
+);
+
+storeRouter.patch(
+  "/:storeUid",
+  validateAuthorization,
+  validateRequestBody(StoreUpdateSchema),
+  storeController.patchStore.bind(storeController)
+);
+
+storeRouter.delete(
+  "/:storeUid",
+  validateAuthorization,
+  storeController.deleteStore.bind(storeController)
+);
+
+storeRouter.get(
+  "/:storeUid",
+  storeController.getStore.bind(storeController)
+);
