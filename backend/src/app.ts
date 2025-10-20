84b99e20d2fda64667320a08e006391d7322c765
Anurag Rajput <code.annu@gmail.com>
2025-10-20 09:19:10 +0530

    Implement buyer feature

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import {errorHandler} from "./api/middleware/handle-error";
 import {authRouter} from "./api/router/signup-router";
+import {buyerRouter} from "./api/router/buyer-router";
 
 const app = express();
 
@@ -9,6 +10,7 @@ const BASE_API_URL = '/api/v1'
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(`${BASE_API_URL}/auth`, authRouter)
+app.use(`${BASE_API_URL}/buyer`, buyerRouter)
 
 
 app.use(errorHandler)
