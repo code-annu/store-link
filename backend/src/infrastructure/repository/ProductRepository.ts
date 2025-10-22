56305e09f626aee34952674ef0f5238cabf753b2
Anurag Rajput <code.annu@gmail.com>
2025-10-22 20:54:34 +0530

    implement product infrastructure layer

---
+++ backend/src/infrastructure/repository/ProductRepository.ts
@@ -0,0 +1,29 @@
+import {BaseRepository} from "./BaseRepository";
+import {Product, ProductCreate, ProductUpdate} from "../../domain/entity/product";
+import {IProductRepository} from "../../domain/repository/IProductRepository";
+
+const PRODUCT_TABLE = 'products';
+
+export class ProductRepository extends BaseRepository<Product> implements IProductRepository {
+    constructor() {
+        super(PRODUCT_TABLE);
+    }
+
+    async createProduct(productCreate: ProductCreate): Promise<Product> {
+        return super.create(productCreate)
+    }
+
+    async deleteProduct(productUid: string): Promise<Product | null> {
+        return super.deleteOne("uid", productUid);
+    }
+
+    async getProduct(productUid: string): Promise<Product | null> {
+        return super.findBy("uid", productUid);
+    }
+
+
+    async updateProduct(productUid: string, updates: ProductUpdate): Promise<Product | null> {
+        return super.updateOne("uid", productUid, updates);
+    }
+
+}
