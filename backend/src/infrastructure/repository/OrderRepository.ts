def03e4919b1fe26943dad41a4d3351ed7c47082
Anurag Rajput <code.annu@gmail.com>
2025-10-23 22:26:00 +0530

    implement order infrastrcuture layer

---
+++ backend/src/infrastructure/repository/OrderRepository.ts
@@ -0,0 +1,33 @@
+import { Order, OrderCreate, OrderUpdate } from "../../domain/entity/order";
+import { IOrderRepository } from "../../domain/repository/IOrderRepository";
+import { BaseRepository } from "./BaseRepository";
+
+const TABLE_ORDER = "orders";
+
+export class OrderRepository
+  extends BaseRepository<Order>
+  implements IOrderRepository
+{
+  constructor() {
+    super(TABLE_ORDER);
+  }
+
+  async createOrder(orderCreate: OrderCreate): Promise<Order> {
+    return super.create(orderCreate);
+  }
+
+  async getOrderByUid(orderUid: string): Promise<Order | null> {
+    return super.findBy("uid", orderUid);
+  }
+
+  async updateOrder(
+    orderUid: string,
+    updates: OrderUpdate
+  ): Promise<Order | null> {
+    return super.updateOne("uid", orderUid, updates);
+  }
+
+  async deleteOrder(orderUid: string): Promise<Order | null> {
+    return super.deleteOne("uid", orderUid);
+  }
+}
