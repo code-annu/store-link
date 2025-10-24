cf5c87e973bbfd6da8ca9deb10dce7829ce75320
Anurag Rajput <code.annu@gmail.com>
2025-10-24 20:06:34 +0530

    implement products domain layer

--- backend/src/domain/repository/IProductRepository.ts
+++ backend/src/domain/repository/IProductRepository.ts
@@ -1,12 +1,20 @@
-import {Product, ProductCreate, ProductUpdate} from "../entity/product";
+import { Product, ProductCreate, ProductUpdate } from "../entity/product";
 
 export interface IProductRepository {
-    createProduct(productCreate: ProductCreate): Promise<Product>;
+  createProduct(productCreate: ProductCreate): Promise<Product>;
 
-    getProduct(productUid: string): Promise<Product | null>;
+  getProduct(productUid: string): Promise<Product | null>;
 
-    deleteProduct(productUid: string): Promise<Product | null>;
+  deleteProduct(productUid: string): Promise<Product | null>;
 
-    updateProduct(productUid: string, updates: ProductUpdate): Promise<Product | null>;
+  updateProduct(
+    productUid: string,
+    updates: ProductUpdate
+  ): Promise<Product | null>;
 
+  getListOfRandomProducts(): Promise<Product[]>;
+
+  searchProducts(query: string): Promise<Product[]>;
+
+  getListOfProducts(category: string): Promise<Product[]>;
 }
